3834d9eebb0b966f102567086b359e2f19ddc7d6
Antonio Ferreira <axd108@gmail.com>
2024-04-01 15:22:25 -0300

    API de grupos para saida

--- source/util.c
+++ source/util.c
@@ -7,7 +7,7 @@
 void load_points(FILE *input){
 	char *line = NULL;
 	size_t size = 0;           // able to store the size of the largest possible object
-	ssize_t line_size;         // can be used to also represent negative numbers
+	size_t line_size;         // can be used to also represent negative numbers
 
 	while((line_size = getline(&line, &size, input)) != -1){
 		printf("%s", line);    // aqui será o local na qual os pontos serão armazenados
