8198213c7fca4201159a77232a0176c974604d11
naascimento0 <gabrieloliveiran@hotmail.com>
2024-04-15 22:08:07 -0300

    updating util.c and util.h

--- headers/util.h
+++ headers/util.h
@@ -2,11 +2,6 @@
 #define UTIL_H_
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-
-int line_file_counter(FILE *file);
 
 int dimension_counter(FILE *file);
 
--- source/util.c
+++ source/util.c
@@ -1,29 +1,6 @@
 #include "../headers/util.h"
 #include "../headers/vertex.h"
 
-#define BUFFER_SIZE 65536
-
-/*
-função retirada do StackOverflow : https://stackoverflow.com/questions/12733105/c-function-that-counts-lines-in-file
-*/
-int line_file_counter(FILE *file){
-    char buffer[BUFFER_SIZE];
-    int counter = 0;
-    while(1){
-        size_t max_size = fread(buffer, 1, BUFFER_SIZE, file);
-        if (ferror(file))
-            return -1;
-
-        int i;
-        for (i = 0; i < max_size; i++)
-            if (buffer[i] == '\n')counter++;
-
-        if (feof(file))
-            break;
-    }
-    return counter;
-}
-
 int dimension_counter(FILE *file){
     size_t size = 0;          
 	char *line = NULL;
