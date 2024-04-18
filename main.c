30a57b73e7b435e2d8aacab53981b618cd990a10
PhenBD <pedro.h.duarte@edu.ufes.br>
2024-04-17 22:11:28 -0300

    retira o time.h da main

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>	
 #include "headers/graph.h"
 
 int main(int argc, char **argv){
@@ -16,16 +15,11 @@ int main(int argc, char **argv){
 	if(!input)
 		exit(printf("ERROR: File %s did not open", input_file_path));
 
-	clock_t start = clock();
 	Graph *graph = graph_create(input);
 	graph_msca(graph, k, output_file_path);
-	clock_t end = clock();
 	graph_destroy(graph);
 
 	fclose(input);
-
-	double seconds = ((double) end - start) / CLOCKS_PER_SEC;
-    printf ("\n tempo gasto: %lf \n ", seconds);
 
 	return 0;
 }
